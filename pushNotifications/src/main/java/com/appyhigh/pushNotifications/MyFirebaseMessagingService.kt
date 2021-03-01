a09ceb88f7d3df1a4083a568d03c54215d8dd1fb
tejaSomanchi <teja@appyhigh.com>
2021-03-01 16:29:25 +0530

    Fix  missedNotifications

--- pushNotifications/src/main/java/com/appyhigh/pushNotifications/MyFirebaseMessagingService.kt
+++ pushNotifications/src/main/java/com/appyhigh/pushNotifications/MyFirebaseMessagingService.kt
@@ -686,6 +686,7 @@ class MyFirebaseMessagingService : FirebaseMessagingService(),InAppNotificationB
                     Log.d(TAG, "fetchNotifications error: "+it.message)
                 })
         } catch (e: Exception) {
+            Log.d(TAG, "fetchNotifications: catch "+e.printStackTrace())
             e.printStackTrace()
         }
 
@@ -995,7 +996,7 @@ class MyFirebaseMessagingService : FirebaseMessagingService(),InAppNotificationB
                 }
             }
         } catch (e: Exception) {
-            Log.e(TAG, "checkForInAppNotifications: \$e")
+            Log.e(TAG, "checkForInAppNotifications: $e")
 //                Dont push
         }
     }
